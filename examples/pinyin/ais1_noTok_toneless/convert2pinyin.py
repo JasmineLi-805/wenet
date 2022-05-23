1c8fa8536477d87cebf6c8e7a5acd0436a4cb8df
Jasmine Li <jasminel.lyr@gmail.com>
2022-05-23 14:55:27 -0700

    remove space in text

--- examples/pinyin/ais1_noTok_toneless/convert2pinyin.py
+++ examples/pinyin/ais1_noTok_toneless/convert2pinyin.py
@@ -22,6 +22,7 @@ with open(read_file_name, 'r') as file:
 
         pred = pypinyin.pinyin(character, style=pypinyin.Style.NORMAL, neutral_tone_with_five=True)
         pred = [p[0] for p in pred]
+        pred = ''.join(pred)
         pred = pred.upper()
         # print(pred)
 
